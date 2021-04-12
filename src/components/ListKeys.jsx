22d82bf43bed10f7221f6feac66ce443eef496a5
Callan Swanson <callanswanson94@gmail.com>
2021-04-12 21:22:47 +0100

    refresh key

--- src/components/ListKeys.jsx
+++ src/components/ListKeys.jsx
@@ -4,6 +4,7 @@ import useStore from "../store"
 const Component = () => {
     const savedKeys = useStore(state => state.keys)
     const deleteKey = useStore(state => state.deleteKey)
+    const refreshKey = useStore(state => state.refreshKey)
 
     return (
         <table>
@@ -22,6 +23,9 @@ const Component = () => {
                         <td>
                             <button onClick={() => deleteKey(key.name)}>
                                 Delete
+                            </button>
+                            <button onClick={() => refreshKey(key.name)}>
+                                Refresh
                             </button>
                         </td>
                     </tr>
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,20 @@ import { persist } from "zustand/middleware"
 import fetchAccountAchievements from "../api/accountAchievements"
 
 export const useStore = create(persist(
-    (set) => ({
+    (set, get) => ({
         keys: [],
         saveNewKey: async (newKey) => {
             const accountAchievements = await fetchAccountAchievements({ accountKey: newKey.value })
             set(state => ({ keys: [...state.keys, { ...newKey, achievements: accountAchievements } ] }))
         },
-        deleteKey: (keyName) => set(state => ({ keys: state.keys.filter(key => key.name !== keyName) }))
+        deleteKey: (keyName) => set(state => ({ keys: state.keys.filter(key => key.name !== keyName) })),
+        refreshKey: async (keyName) => {
+            const keyEntry = get().keys.findIndex(k => k.name === keyName)
+            const accountAchievements = await fetchAccountAchievements({ accountKey: get().keys[keyEntry].value })
+            const newKeys = get().keys
+            newKeys[keyEntry].achievements = accountAchievements
+            set(newKeys)
+        }
     }),
     {
         name: "key-storage", // unique name
